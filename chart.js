bae97d6c6503b130c101420a17fe10d997d9e5aa
Anna Krylova <annakry@wix.com>
2020-05-12 23:14:48 +0300

    Fixes to pass tests

--- chart.js
+++ chart.js
@@ -1,10 +1,12 @@
 function drawChart(data) {
-    const margin = { top: 40, right: 50, bottom: 60, left: 60 };
-    const w = 800;
+    // const margin = { top: 40, right: 50, bottom: 60, left: 60 };
+    var margin = { top: 100, right: 20, bottom: 30, left: 60 };
+    const w = 920;
     const h = 630;
 
     const chartWidth = w - margin.left - margin.right;
     const chartHeight = h - margin.top - margin.bottom;
+
 
     const rectWidth = w / data.length;
     // let color = d3.scaleOrdinal(d3.schemeCategory10);
@@ -28,9 +30,10 @@ function drawChart(data) {
         .tickFormat(d3.format("d"))
 
     svg.append('g')
-        .attr('transform', `translate(0, ${chartHeight + 4})`)
+        .attr('transform', `translate(0, ${chartHeight})`)
         .call(x)
-        .attr("id", "x-axis");
+        .attr("id", "x-axis")
+        .attr("class", "label");
 
     // render yAxis
     var specifier = "%M:%S";
@@ -47,11 +50,10 @@ function drawChart(data) {
         .tickFormat(d3.timeFormat(specifier))
 
     svg.append('g')
-        .attr('transform', `translate(0, 4)`)
+        .attr('transform', `translate(0, 0)`)
         .call(y)
         .attr("id", "y-axis")
-        .append("text")
-        ;
+        .attr("class", "label");
 
     svg.selectAll(".dot")
         .data(data)
@@ -61,18 +63,18 @@ function drawChart(data) {
         .attr("cx", d => xScale(d.Year))
         .attr("cy", (d, i) => yScale(times[i]))
         .attr("data-xvalue", d => d.Year)
-        .attr("data-yvalue", (d, i) => times[i].getMinutes())
+        .attr("data-yvalue", (d, i) => times[i].toISOString())
         .style("fill", d => color(d.Doping != ""))
         .on("mouseover", function (d) {
             tooltip.transition()
                 .duration(200)
-                .style("opacity", 0.9);
+                .style("opacity", 0.9)
+                .attr("data-year", d.Year);
             tooltip.html(d.Name + ": " + d.Nationality + "<br/>"
                 + "Year: " + d.Year + ", Time: " + d.Time + "<br/>" + "<br/>"
                 + d.Doping)
                 .style("left", (d3.event.pageX + 10) + "px")
                 .style("top", (d3.event.pageY - 28) + "px")
-                .attr("data-year", d => d.Year)
         })
         .on("mouseout", function (d) {
             tooltip.transition()
@@ -88,7 +90,7 @@ function drawChart(data) {
         .data(color.domain())
         .enter().append("g")
         .attr("class", "legend-label")
-        .attr("transform", (d, i) => "translate(0," + (chartHeight / 2 - i * 20) + ")");
+        .attr("transform", (d, i) => "translate(0," + (chartWidth / 3 - i * 20) + ")");
 
     legend.append("rect")
         .attr("x", chartWidth - 18)
@@ -98,7 +100,8 @@ function drawChart(data) {
 
     legend.append("text")
         .attr("x", chartWidth - 24)
-        .attr("y", 15)
+        .attr("y", 13)
+        .attr("class", "label")
         .style("text-anchor", "end")
         .text(function (d) {
             if (d) return "Riders with doping allegations";
@@ -116,17 +119,9 @@ function drawChart(data) {
 
     svg.append('text')
         .attr('transform', 'rotate(-90)')
-        .attr('x', -150)
-        .attr('y', -45)
-        .attr("class", "label")
-        .text('Time in Minutes');
-
-
-    svg.append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('x', -150)
-        .attr('y', -45)
-        .attr("class", "label")
+        .attr('x', -160)
+        .attr('y', -44)
+        .attr("class", "axis-label")
         .text('Time in Minutes');
 
 }
